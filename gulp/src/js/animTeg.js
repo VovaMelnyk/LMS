adac463032dd60c45015575bb940fa3e500bdc3f
Vova Grytsun <pmsz@ukr.net>
2017-12-06 13:50:23 +0200

    test

--- gulp/src/js/animTeg.js
+++ gulp/src/js/animTeg.js
@@ -1,12 +1,19 @@
-let main = document.getElementsByClassName('modal-window');
+let modal = document.getElementsByClassName('modal-window');
+
 
 let item1 = ["[password]", "<h>", "<p/>", "<h/>", "<>", "<>"];
 
 
 var W = 500,
   H = 500,
-  R = 50,
+  R = 20,
   S = 2;
+let position = modal[0].getBoundingClientRect();
+console.log(position.width);
+console.log("bottom " +position.bottom);
+console.log("left " +position.left);
+console.log("width " + modal[0].style.width);
+
 
 var balls = [];
 let count = 0;
@@ -15,16 +22,25 @@ function createBall() {
 
   let ball = document.createElement("DIV");
   ball.innerText = item1[count];
+  console.log("randomTop " + item1[count] + " " + (Math.floor((Math.random() * (position.bottom - position.top)) + position.top)-position.top));
   count = count + 1;
-  main[0].appendChild(ball);
-  ball.style.cssText = "top: " + (Math.random() * 400) + "px; left:" + (Math.random() * 400) +
-    "px; position: absolute; color: #3a8a94;  border-radius: " + R + "px";
+  modal[0].appendChild(ball);
+
+  ball.style.cssText = "margin-top: " + (Math.floor(Math.random() * 500 ))  +
+  "px; margin-left:" + (Math.floor(Math.random() * position.width-100))  +
+  "px; " +
+    "px; position: absolute; z-index: -1; width: 30px; color: #3a8a94; background-color: red;  border-radius: " + R + "px";
   ball.direction = Math.random() * 6.28;
   var pos = ball.getBoundingClientRect();
   ball.realTop = pos.top;
   ball.realLeft = pos.left;
+  console.log(ball.getClientRects());
+  console.log("top" + item1[count] + "" + ball.offsetTop);
+  console.log("left real" + item1[count] + "" + ball.offsetLeft);
+
+
   setInterval(function() {
-    moveBall(ball)
+    //moveBall(ball)
   }, 17);
   balls.push(ball);
 }
@@ -33,7 +49,7 @@ function moveBall(ball) {
 
   for (var i = 0; i < balls.length; i++) {
     if (balls[i] == ball || ball.timeout) continue;
-    if (ball.realTop > balls[i].realTop && ball.realTop < (balls[i].realTop + R) && ball.realLeft > balls[i].realLeft && ball.realLeft < (balls[i].realLeft + R)) {
+    if (ball.realTop > balls[i].realTop && ball.realTop < (balls[i].realTop ) && ball.realLeft > balls[i].realLeft && ball.realLeft < (balls[i].realLeft + R)) {
       ball.direction += Math.PI / 2
       balls[i].direction += Math.PI / 2
       ball.timeout = true
@@ -46,7 +62,7 @@ function moveBall(ball) {
   ball.realTop += Math.sin(ball.direction) * S;
   ball.realLeft += Math.cos(ball.direction) * S;
 
-  if (ball.realTop + R > H || ball.realLeft + R > W || ball.realTop < 0 || ball.realLeft < 0) {
+  if (ball.realTop  > position.bottom+position.top || ball.realLeft > position.right || ball.realTop < ball.offsetTop || ball.realLeft < ball.offsetLeft) {
     ball.direction += Math.PI / 2;
     return moveBall(ball);
   }
@@ -55,5 +71,5 @@ function moveBall(ball) {
 }
 
 
-var i = 5;
+var i = 4;
 while (i--) createBall();
