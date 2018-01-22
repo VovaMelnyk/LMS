31b77cf38e1f16ce35625958f9d1b1bf6cfc823d
Elena Bazaluk <bazik111@ukr.net>
2018-01-23 00:11:52 +0200

    scroll

--- gulp/src/js/m-left-nav.js
+++ gulp/src/js/m-left-nav.js
@@ -2,6 +2,7 @@ function hideLeftContentMenu() {
   let pageY = window.pageYOffset || document.documentElement.scrollTop;
   let  innerHeight = document.documentElement.clientHeight;
   let textToHide = document.querySelectorAll('.m-left-nav__text');
+  let contentBlock = document.querySelector('.m-left-nav');
       // if (pageY > 300) {
       //   textToHide.forEach(function (elem){
       //       elem.classList.add('invisible');
@@ -13,6 +14,7 @@ function hideLeftContentMenu() {
       textToHide.forEach(function (elem){
         elem.classList.add('invisible');
         });
+        contentBlock.classList.add('width');
       }
 
   }
