2c09699ddd11bc988582211ea47d18728fbf8d31
Nataliia Nechiporenko <nataliia.nechiporenko@gmail.com>
2018-01-23 00:55:29 +0200

    Made password validation

--- gulp/src/js/check_input_fields.js
+++ gulp/src/js/check_input_fields.js
@@ -6,10 +6,12 @@ function checkRegistrationFields() {
     let name = select("#name").value;
     let surname = select("#surname").value;
     let email = select("#new-email").value;
+    let password = select("#new_pass").value;
     let errorMessage = select("#error-message");
     let inputEmail = select('.input-wrapper-email');
     let inputName = select(".input-wrapper-name");
     let inputSurname = select(".input-wrapper-surname");
+    let inputPassword = select(".input-wrapper-pass");
     let nameString = "";
     let surnameString = "";
     let emailString = "";
@@ -17,10 +19,12 @@ function checkRegistrationFields() {
     let namePattern = /^[A-Z]{1}([^а-яёєіїґ’'`]i?)[a-z]+((\s[A-Z]{1}([^а-яёєіїґ’'`]i?)[a-z]+)+)?$|^[А-ЯЁ]{1}([^a-zєіїґ’'`]i?)[а-яё]+((\s[А-ЯЁ]{1}([^a-zєіїґ’'`]i?)[а-яё]+)+)?$|^[А-ЯЄІЇҐ’'`]{1}([^a-zыэъ]i?)[а-яєіїґ’'`]+((\s[А-ЯЄІЇҐ’'`]{1}([^a-zыэъ]i?)[а-яєіїґ’'`]+)+)?$/;
     let emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,6})+$/;
     let surnamePattern = /^[A-Z]{1}([^а-яёєіїґ’'`]i?)[a-z]+((-[A-Z]{1}([^а-яёєіїґ’'`]i?)[a-z]+)+)?$|^[А-ЯЁ]{1}([^a-zєіїґ’'`]i?)[а-яё]+((-[А-ЯЁ]{1}([^a-zєіїґ’'`]i?)[а-яё]+)+)?$|^[А-ЯЄІЇҐ’'`]{1}([^a-zыэъ]i?)[а-яєіїґ’'`]+((-[А-ЯЄІЇҐ’'`]{1}([^a-zыэъ]i?)[а-яєіїґ’'`]+)+)?$/;
+    let passwordPattern = /\s+/;
 
     let checkEmail = emailPattern.test(email);
     let checkName = namePattern.test(name);
-    let checkSurname = surnamePattern.test(surname);    
+    let checkSurname = surnamePattern.test(surname);
+    let checkPassword = !(passwordPattern.test(password)||password.length<4||password.length>32);
 
     if (!checkName) {
         inputName.classList.add("input-wrapper--wrong-data");
@@ -38,13 +42,20 @@ function checkRegistrationFields() {
 
     if (!checkEmail){
         inputEmail.classList.add("input-wrapper--wrong-data");
-        emailString = "Email";
+        emailString = "E-mail, ";
     } else {
         inputEmail.classList.remove("input-wrapper--wrong-data");
     }
 
-    if (!(checkName && checkSurname && checkEmail)) {
-        errorMessage.textContent = `Неверно введены: ${nameString} ${surnameString} ${emailString}`;
+    if (!checkPassword){
+        inputPassword.classList.add("input-wrapper--wrong-data");
+        passwordString = "пароль, ";
+    } else {
+        inputPassword.classList.remove("input-wrapper--wrong-data");
+    }
+
+    if (!(checkName && checkSurname && checkEmail && checkPassword)) {
+        errorMessage.textContent = `Неверно введены: ${nameString} ${surnameString} ${emailString} ${passwordString}`;
     } else {
         errorMessage.textContent = "";
     }
