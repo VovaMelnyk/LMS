189042efbdb364be89b52888fbca9411239de2c5
y_tychinskaya <juljul1@gmail.com>
2018-01-23 19:48:28 +0200

    added prompt

--- gulp/src/js/m-left-nav.js
+++ gulp/src/js/m-left-nav.js
@@ -3,19 +3,17 @@ function hideLeftContentMenu() {
   let  innerHeight = document.documentElement.clientHeight;
   let textToHide = document.querySelectorAll('.m-left-nav__text');
   let contentBlock = document.querySelector('.m-left-nav');
-      // if (pageY > 300) {
-      //   textToHide.forEach(function (elem){
-      //       elem.classList.add('invisible');
-      //   }
-      // }
-      if (pageY > 300) {
-      console.log (pageY);
-      console.log(textToHide);
-      textToHide.forEach(function (elem){
-        elem.classList.add('invisible');
-        });
-        contentBlock.classList.add('width');
-      }
 
+     if (pageY > 300) {
+        textToHide.forEach(function (elem){
+            elem.classList.add('invisible');
+        });
+    contentBlock.classList.add('width');
+    }else if (pageY < 300) {
+        textToHide.forEach(function (elem){
+            elem.classList.remove('invisible');
+        });
+        contentBlock.classList.remove('width');
+    }
   }
 document.addEventListener('scroll', ()=>hideLeftContentMenu());
