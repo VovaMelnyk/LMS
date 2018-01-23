be9f01ec064d0bc404ea9c2c83f0f161be481dec
Miniaylo.S <po4tovuj@gmail.com>
2018-01-23 16:37:30 +0200

    separate check Fn

---
+++ gulp/src/js/chekSeparate.js
@@ -0,0 +1,71 @@
+function select(name) {
+    return document.querySelector(name);
+}
+
+let checkRegistrFields = {
+    nameString: "",
+    surnameString: "",
+    emailString: "",
+    passwordString: "",
+    errorMessage: select("#error-message"),
+    checkRegName: function () {
+        let name = select("#name").value;
+        let namePattern = /^[A-Z]{1}([^а-яёєіїґ’'`]i?)[a-z]+((\s[A-Z]{1}([^а-яёєіїґ’'`]i?)[a-z]+)+)?$|^[А-ЯЁ]{1}([^a-zєіїґ’'`]i?)[а-яё]+((\s[А-ЯЁ]{1}([^a-zєіїґ’'`]i?)[а-яё]+)+)?$|^[А-ЯЄІЇҐ’'`]{1}([^a-zыэъ]i?)[а-яєіїґ’'`]+((\s[А-ЯЄІЇҐ’'`]{1}([^a-zыэъ]i?)[а-яєіїґ’'`]+)+)?$/;
+        let checkName = namePattern.test(name);
+        if (!checkName) {
+            select(".input-wrapper-name").classList.add("input-wrapper--wrong-data");
+            this.nameString = "Имя,";
+        } else {
+            this.nameString = "";
+            select(".input-wrapper-name").classList.remove("input-wrapper--wrong-data");
+        }
+        return checkName;
+    },
+    checkRegSurname: function () {
+        let surname = select("#surname").value;
+        let surnamePattern = /^[A-Z]{1}([^а-яёєіїґ’'`]i?)[a-z]+((-[A-Z]{1}([^а-яёєіїґ’'`]i?)[a-z]+)+)?$|^[А-ЯЁ]{1}([^a-zєіїґ’'`]i?)[а-яё]+((-[А-ЯЁ]{1}([^a-zєіїґ’'`]i?)[а-яё]+)+)?$|^[А-ЯЄІЇҐ’'`]{1}([^a-zыэъ]i?)[а-яєіїґ’'`]+((-[А-ЯЄІЇҐ’'`]{1}([^a-zыэъ]i?)[а-яєіїґ’'`]+)+)?$/;
+        let checkSurname = surnamePattern.test(surname);
+        if (!checkSurname) {
+            select(".input-wrapper-surname").classList.add("input-wrapper--wrong-data");
+            this.surnameString = "Фамилия,";
+        } else {
+            this.surnameString = "";
+            inputSurname.classList.remove("input-wrapper--wrong-data");
+        }
+        return checkSurname;
+    },
+    checkRegEmail: function () {
+        let email = select("#new-email").value;
+        let emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,6})+$/;
+        let checkEmail = emailPattern.test(email);
+        if (!checkEmail) {
+            select('.input-wrapper-email').classList.add("input-wrapper--wrong-data");
+            this.emailString = "E-mail, ";
+        } else {
+            this.emailString = '';
+            inputEmail.classList.remove("input-wrapper--wrong-data");
+        }
+        return checkEmail;
+    },
+    checkRegPass: function () {
+        let password = select("#new_pass").value;
+        let passwordPattern = /\s+/;
+        let checkPassword = !(passwordPattern.test(password) || password.length < 4 || password.length > 32);
+        if (!checkPassword) {
+            select(".input-wrapper-pass").classList.add("input-wrapper--wrong-data");
+            this.passwordString = "Пароль.";
+        } else {
+            this.passwordString = "";
+            select(".input-wrapper-pass").classList.remove("input-wrapper--wrong-data");
+        }
+        return checkPassword;
+    },
+
+    checkValid: function () {
+        if (!(this.checkRegName && this.checkRegSurname && this.checkRegEmail && this.checkRegPassword)) {
+            this.errorMessage.textContent = `Неверно введены: ${this.nameString} ${this.surnameString} ${this.emailString} ${this.passwordString}`;
+        } else {
+            this.errorMessage.textContent = "";
+        }
+    }
+};
