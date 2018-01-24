5baac351c126d5a7f9a53f53957fce719751504b
Vladislav <marklein.work@gmail.com>
2018-01-24 19:03:52 +0200

    debuged right-navbar position

--- gulp/src/js/line-indicator.js
+++ gulp/src/js/line-indicator.js
@@ -12,14 +12,24 @@
 
         width = `width: ${Math.round(scrolledRange / crollingHeight * 100)}%`;
         background = `background: rgb(${color})`;
-        
+
         viewNode.setAttribute('style', `${width};${background}`)
     }
-    
+
     function init () {
         const scrollLine = document.getElementById('scroll-indicator');
+        const rightNav = document.querySelector('.right-nav');
         showScrollWidth(scrollLine)
-        document.addEventListener('scroll', ()=>showScrollWidth(scrollLine))
+        document.addEventListener('scroll', (e) => {
+            console.log(e.path[1].scrollY);
+            if(e.path[1].scrollY > 228) {
+                rightNav.classList.add('right-nav-fixed');
+            }
+            if (e.path[1].scrollY < 228) {
+                rightNav.classList.remove('right-nav-fixed')
+            };
+            showScrollWidth(scrollLine)
+        })
     }
 
     document.addEventListener('DOMContentLoaded', init)
