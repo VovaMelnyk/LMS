db562b77187eba403c4c7b7673bd66b1e5aed90e
y_tychinskaya <juljul1@gmail.com>
2018-01-25 17:31:53 +0200

    add left content & changed position

--- gulp/src/js/line-indicator.js
+++ gulp/src/js/line-indicator.js
@@ -19,14 +19,31 @@
     function init () {
         const scrollLine = document.getElementById('scroll-indicator');
         const rightNav = document.querySelector('.right-nav');
+        const textToHideLeftContent = document.querySelectorAll('.m-left-nav__text');
+        const blockLinkLeftCont = document.querySelectorAll('.m-left-nav__link');
+        const contentBlockLeft = document.querySelector('.m-left-nav');
         showScrollWidth(scrollLine)
         document.addEventListener('scroll', (e) => {
             console.log(e.path[1].scrollY);
             if(e.path[1].scrollY > 228) {
                 rightNav.classList.add('right-nav-fixed');
+                contentBlockLeft.classList.add('m-left-nav-fixed');
+                textToHideLeftContent.forEach(function (elem){
+                    elem.classList.add('invisible');
+                });
+                blockLinkLeftCont.forEach(function (elem){
+                    elem.classList.add('m-left-nav__link-fixed');
+                });
             }
             if (e.path[1].scrollY < 228) {
-                rightNav.classList.remove('right-nav-fixed')
+                rightNav.classList.remove('right-nav-fixed');
+                contentBlockLeft.classList.remove('m-left-nav-fixed');
+                textToHideLeftContent.forEach(function (elem){
+                    elem.classList.remove('invisible');
+                });
+                blockLinkLeftCont.forEach(function (elem){
+                    elem.classList.remove('m-left-nav__link-fixed');
+                });
             };
             showScrollWidth(scrollLine)
         })
