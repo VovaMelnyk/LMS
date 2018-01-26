fc58815e002fa45adcb10674b253100cb9cd4c62
Stanislav Bereza <stanislav.bereza@gmail.com>
2018-01-26 16:54:44 +0200

    added js file

--- gulp/src/js/script.js
+++ gulp/src/js/script.js
@@ -1,3 +1,14 @@
-    document.getElementsByClassName('link-calendar').onclick =  function () {
-    document.getElementById('alerts-calendar').style.zIndex = '999';
-    };
+
+    let navBarIcon = document.getElementsByClassName('c-header__icon');
+
+    for (let i = 0; i < navBarIcon.length; i++) {
+    	navBarIcon[i].onclick = displayNotifications;
+    }
+
+    function displayNotifications() {
+        let notifications = document.getElementsByClassName('c-notifications');
+        for (let i = 0; i < notifications.length; i++) {
+            notifications[i].style.display = 'block';
+        }
+        console.log(notifications[0]);
+    }
