b886435141f21f9913ae99ae4e7bb6f3d485f61e
Stanislav Bereza <stanislav.bereza@gmail.com>
2018-01-29 01:19:18 +0200

    updated js

--- gulp/src/js/script.js
+++ gulp/src/js/script.js
@@ -1,10 +1,30 @@
 
-    let navBarIcon = document.getElementsByClassName('c-header__icon');
+    let navBarIcon = document.getElementsByClassName('c-header__icon-nonclicked');
+    let i;
 
     for (let i = 0; i < navBarIcon.length; i++) {
-    	navBarIcon[i].onclick = displayNotifications;
-    }
-
+    	navBarIcon[i].addEventListener('click', function () {
+            console.log(this);
+            let notification = document.getElementsByClassName('c-notifications');
+            //console.log(notification);
+            let notifications = this.nextElementSibling;
+            //console.log(this.nextElementSibling);
+                 //for (let i = 0; i < notifications.length; i++) {
+                    if (notifications.style.display === 'block') {
+                        notifications.style.display = 'none';
+                        //this.classList.remove('c-header__icon-clicked');
+                        this.classList.add('c-header__icon-nonclicked');
+                        this.classList.toggle('c-header__icon-clicked');
+                    } else {
+                        notifications.style.display = 'block';
+                        this.classList.remove('c-header__icon-nonclicked');
+                        //this.classList.add('c-header__icon-clicked');
+                        this.classList.toggle('c-header__icon-clicked');
+                    }
+            //}
+        });
+        }
+/*
     function displayNotifications() {
         let notifications = document.getElementsByClassName('c-notifications');
         for (let i = 0; i < notifications.length; i++) {
@@ -12,3 +32,4 @@
         }
         console.log(notifications[0]);
     }
+*/
