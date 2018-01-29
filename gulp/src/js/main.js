f45548461f6b98d057b7356082bfe1443f71ac69
Sergii Tymchenko <p70smart@gmail.com>
2018-01-29 13:15:20 +0200

    correction

--- gulp/src/js/main.js
+++ gulp/src/js/main.js
@@ -1,6 +1,7 @@
-let photoProfile = document.getElementsByClassName('c-header__photo-profile');
-let notifications = document.getElementsByClassName('c-photo-profile__notifications');
 
-photoProfile.addEventListener("click", function(){
+
+window.addEventListener("click", function(){
+	let photoProfile = document.getElementsByClassName('c-header__photo-profile');
+	let notifications = document.getElementsByClassName('c-photo-profile__notifications');
 	notifications[0].style.display = "block";
 });
