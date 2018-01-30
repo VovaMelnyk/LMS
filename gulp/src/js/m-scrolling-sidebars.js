b58bdb9aef9f7716b19fac82fa47c9d26744b755
yevheniigurinets <yevheniigurinets@gmail.com>
2018-01-31 00:26:14 +0200

    menu changed

--- gulp/src/js/m-scrolling-sidebars.js
+++ gulp/src/js/m-scrolling-sidebars.js
@@ -2,17 +2,13 @@
 
     function hidePrompts (nodeArr) {
         [].forEach.call(nodeArr,(item)=>{
-            item.setAttribute('style', 'display: none;')
+            item.setAttribute('style', 'width: 0px; opacity: 0;')
         })
-    }
-
-    function hideNavItems (parentNode) {
-        parentNode.classList.add('hidden')
     }
 
     function positionCorrecting (node, parentHeight) {
         let top = -parentHeight + 200;
-        node.setAttribute('style', ` height: 30px; top: ${top}px;`)
+        node.setAttribute('style', `top: ${top}px;`)
     }
 
     function materialBarsPositionCheker (parentNode, navRightNode, navLeftNode) {
@@ -31,38 +27,21 @@
         if (pagePosition > 0) {
             setTimeout( () => {
                 hidePrompts(nodeArr)
-                hideNavItems(leftNavBlock)
             }, 3000 )
         } else {
             hidePrompts(nodeArr)
-            hideNavItems(leftNavBlock)
         }
     }
 
-    function leftNavHeightCheker (node) {
-        const chilSum = node.children.length;
-
-        switch (node.style.height){
-            case '30px':
-                node.style.height = `${chilSum * 30}px`
-                setTimeout( ()=> node.style.overflow = 'inherit', 500)
-                break
-            case `${chilSum * 30}px`:
-                node.style.height = '30px';
-                node.style.overflow = 'hidden';
-
-        }
-    }
 
     function init() {
         const rightNavPrompts = document.getElementsByClassName('m-left-nav__prompt'),
             leftNav = document.getElementById('m-left-nav'),
             rightNav = document.getElementById('right-nav'),
-            navParentNode = document.getElementById('main'),
-            leftNavBtn = document.getElementById('m-left-nav--btn');
+            navParentNode = document.getElementById('main');
                 
         chekPagePosition(navParentNode, rightNavPrompts, leftNav)
-        leftNavBtn.addEventListener('click', () => leftNavHeightCheker(leftNav))
+
         document.addEventListener('scroll', () => materialBarsPositionCheker(navParentNode, rightNav, leftNav) )
     }
 
