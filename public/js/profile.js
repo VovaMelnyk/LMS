0e3a0d094ff1176c2455aad9830f1707fa9768c7
Vladyslav Lubeznyy <avinean@gmail.com>
2018-02-04 00:23:02 +0300

    temp

--- public/js/profile.js
+++ public/js/profile.js
@@ -9,8 +9,14 @@ if (!localStorage.getItem('lms_img') &&
 var img = localStorage.getItem('lms_img');
 var name = localStorage.getItem('lms_name');
 var email = localStorage.getItem('lms_email');
+var facebook = localStorage.getItem('lms_facebook');
+var google = localStorage.getItem('lms_google');
+var linkedin = localStorage.getItem('lms_linkedin');
 
 document.querySelector('#profile_photo').setAttribute('src', 'img/users/' + img);
 document.querySelector('#header_photo-profile').setAttribute('src', 'img/users/' + img);
 document.querySelector('#user-name').innerHTML = name + '&#8194;';
-document.querySelector('#user-email').innerHTML = email;
+document.querySelector('#user-email').innerHTML = email; 
+document.querySelector('#user-facebook').setAttribute('href', facebook); 
+document.querySelector('#user-google').setAttribute('href', google); 
+document.querySelector('#user-linkedin').setAttribute('href', linkedin); 
