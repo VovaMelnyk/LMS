29d8385cfa3f17b2de11233da1687fe30341f795
Vladyslav Lubeznyy <avinean@gmail.com>
2018-02-03 23:12:03 +0300

    q

--- public/js/authorization.js
+++ public/js/authorization.js
@@ -1,7 +1,17 @@
+if (
+    localStorage.getItem('lms_name') &&
+    localStorage.getItem('lms_email') &&
+    localStorage.getItem('lms_img') &&
+    localStorage.getItem('lms_pass') &&
+    localStorage.getItem('lms_group')
+    ) {
+    window.location = 'http://localhost:3000/profile.html';
+}
+
 document.querySelector('#gotoprofile').addEventListener('click', function () {
     var email = document.querySelector('#email').value;
     var pass = document.querySelector('#pass').value;
-    axios('http://localhost:3000/users?email=' + email)
+    axios('http://localhost:3000/users?email=' + email + '&pass' + pass)
         .then(function (data) {
 
             if (!data.data[0]) {
@@ -12,6 +22,7 @@ document.querySelector('#gotoprofile').addEventListener('click', function () {
             localStorage.setItem('lms_name', data.data[0].name);
             localStorage.setItem('lms_email', data.data[0].email);
             localStorage.setItem('lms_img', data.data[0].img);
-            localStorage.setItem('lms_hash', data.data[0].hash);
+            localStorage.setItem('lms_pass', data.data[0].pass);
+            localStorage.setItem('lms_group', data.data[0].group);
         });
 });
