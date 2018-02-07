2486815afbef2e1a479c3b6017ee252b27b4afda
y_tychinskaya <juljul1@gmail.com>
2018-02-08 01:13:13 +0200

    corrected API

--- gulp/src/js/m_fetch.js
+++ gulp/src/js/m_fetch.js
@@ -1,7 +1,7 @@
 const theoryName = document.querySelector("#theory-name");
 const theoryNumber = document.querySelector(".caption__theme");
 const contentParent = document.querySelector(".m-content");
-const leftNavParent = document.querySelector("#m_left-nav");
+const leftNavParent = document.querySelector(".m-left-nav");
 
 fetch("http://localhost:3000/course")
   .then(response => {
@@ -14,19 +14,20 @@ fetch("http://localhost:3000/course")
   .then(data => {
     console.log(data);
 
-    theoryName.innerHTML = `${data[0].theory.title}`;
+    theoryName.innerHTML = `[ ${data[0].theory.title} _]`;
     theoryNumber.innerHTML = `ТЕМА ${data[0].theory.number}`;
     let contentCard = "";
-    data[0].theory.content.forEach(elem => {
+    data[0].theory.content.forEach((elem, idx) => {
       contentCard += `
-      <h4 class="m-content__content-title">[ ${elem.contentTitle} _]</h4>
+      <h4 class="m-content__content-title" id="cnt${idx}">[ ${elem.contentTitle} _]</h4>
       <p class="m-content__text">${elem.contentText}</p>
+      <a href="#m-title" class="m-btn-up invisible"> </a>
       `;
     });
     contentParent.innerHTML = contentCard;
     let navCard = "";
     data[0].theory.content.forEach((elem, idx) => {
-      navCard += `<a href="#" class="m-left-nav__link">
+      navCard += `<a href="#cnt${idx}" class="m-left-nav__link">
         ${idx + 1}
         <span class="m-left-nav__prompt">[ ${elem.contentTitle} _]</span>
     </a>
