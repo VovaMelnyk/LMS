bd281e989b814bd1445f067b1d18e9c3cb4f7e75
Vova Grytsun <pmsz@ukr.net>
2018-02-09 12:43:55 +0200

    task 1.2

--- gulp/src/js/pass-test.js
+++ gulp/src/js/pass-test.js
@@ -3,9 +3,9 @@
 // Прописать алгоритм: СТУДЕНТ открывает тест, сервер отдает ему данные, и студент видит тест.
 // При клике студента на ЗАВЕРШИТЬ ТЕСТ система подсчитывает кол-во правильных ответов и выдает баллы.
 
-
+"use strict";
 ///////////////////Globals/////////////////////////
-const url=`http://localhost:3000/tests`;
+const url = `http://localhost:3000/tests`;
 
 const testBtnStart = document.querySelector('#start_test'); // кнопка начать тест на первой стр
 
@@ -15,119 +15,132 @@ const taskScript = document.querySelector('#tests-task'); // script tests-task
 const resultScript = document.querySelector('#tests-result'); // script tests-result
 ///////////////////////////////////////////////////
 
-const updateView = (tests, container, script) => {    // обновляем данные
+const updateView = (tests, container, script) => { // обновляем данные
 
 
 
-    const compiled = _.template(script.textContent.trim());
-    const result = compiled(tests);
-     // это вопрос 4 правильный ответ 2 !!!
-    container.innerHTML = result;
-    let nowId = tests.id
-    if (localStorage.getItem(nowId)) {
-      let rezTest = localStorage.getItem(nowId);
+  const compiled = _.template(script.textContent.trim());
+  const result = compiled(tests);
+  // это вопрос 4 правильный ответ 2 !!!
+  container.innerHTML = result;
+  let nowId = tests.id;
+  if (localStorage.getItem(nowId)) {
+    let rezTest = localStorage.getItem(nowId);
 
-      let lab = document.getElementsByClassName("t-answers__label");
-      for (var i = 0; i < lab.length; i++) {
-        if (rezTest==lab[i].innerHTML) {
-          console.log(lab[i].previousSibling);
-          let check = document.getElementsByClassName('t-answers__item');
-          check[i].checked = "true";
-        } else {
-          console.log("нет совпадения " );
-        }
+    let lab = document.getElementsByClassName("t-answers__label");
+    for (var i = 0; i < lab.length; i++) {
+      if (rezTest == lab[i].innerHTML) {
+        addBlue();
+        console.log(lab[i].previousSibling);
+        let check = document.getElementsByClassName('t-answers__item');
+        check[i].checked = "true";
 
+      } else {
+        console.log("нет совпадения ");
       }
-
-
-
     }
-
+  }
 };
 
+function addBlue() {
+const pageNumder = document.getElementsByClassName("t-navigation__number");
+
+for (var q = 0; q < pageNumder.length; q++) {
+console.log("Yjvth ntcnf "+pageNumder[q]);
+if (localStorage.getItem(pageNumder[q])) {
+pageNumder[q].classList.add("t-navigation__number--blue");
+console.log(pageNumder[q]);
+}
+
+}
+};
 
 let someId = 1;
 let url2 = url + "/" + someId;
 
-function getTestJson(c) {    // Получаем данные с сервера и передаем их функции updateView
-    fetch(c)
-        .then(response => {
-            if (response.ok) return response.json();
-            throw new Error("Error");
-        })
-        .then(data => {
-            updateView(data, taskContainer, taskScript);
+function getTestJson(c) { // Получаем данные с сервера и передаем их функции updateView
+  fetch(c)
+    .then(response => {
+      if (response.ok) return response.json();
+      throw new Error("Error");
+    })
+    .then(data => {
+      updateView(data, taskContainer, taskScript);
+      getLenghtTests(url);
 
-            return data;
 
-        })
-        .then(data => {
-          const btnNext = document.getElementsByClassName("t-navigation__next");
-          const btnPrev = document.getElementsByClassName("t-navigation__prev");
+      return data;
 
-          btnNext[0].addEventListener('click', function () {
-            let newId = data.id + 1;
-            if (newId<=7) {
-              checkratio(data);
+    })
+    .then(data => {
 
-              let urlNext = url + "/" + newId;
+      const btnNext = document.getElementsByClassName("t-navigation__next");
+      const btnPrev = document.getElementsByClassName("t-navigation__prev");
+
+
+      btnNext[0].addEventListener('click', function() {
+        let newId = data.id + 1;
+        if (newId <= 7) {
+          checkratio(data);
+          let urlNext = url + "/" + newId;
           getTestJson(urlNext);
-            }
-          });
-          btnPrev[0].addEventListener('click', function () {
-            let newId = data.id - 1;
-            if (newId>0) {
-              checkratio(data);
-              let urlNext = url + "/" + newId;
+        }
+      });
+      btnPrev[0].addEventListener('click', function() {
+        let newId = data.id - 1;
+        if (newId > 0) {
+          checkratio(data);
+          let urlNext = url + "/" + newId;
           getTestJson(urlNext);
-            }
+        }
 
-          });
-            const testBtnEnd = document.querySelector('#end_test');
-            testBtnEnd.addEventListener('click', ()=>resultTest(data));
-        })
-        .catch(error => {
-            console.error("Error: ", error);
-        });
+      });
+
+
+
+      const testBtnEnd = document.querySelector('#end_test');
+      testBtnEnd.addEventListener('click', () => resultTest(data));
+    })
+    .catch(error => {
+      console.error("Error: ", error);
+    });
 }
 
 testBtnStart.addEventListener('click', function functionName() {
   getTestJson(url2);
 });
-////////////////////////////// попытка сделать сохранение позиции чекбокса/////////////////////
 
 //////////////////////////////если нажата завершить тест////////////////////////
 const resultTest = (templateData) => {
 
-    let answers = document.querySelectorAll('.t-answers__item'); // набор всех вопросов (radio)
-    let arrRadio = Array.from(answers); // массив из radio
+  let answers = document.querySelectorAll('.t-answers__item'); // набор всех вопросов (radio)
+  let arrRadio = Array.from(answers); // массив из radio
   localStorage.clear();
 
-    updateView(templateData, taskContainer, resultScript); // загрузка правильных и неправильных ответов
+  updateView(templateData, taskContainer, resultScript); // загрузка правильных и неправильных ответов
 
-    let mistake = document.querySelector('.t-mistakes__numbers'); //span
-    let question = document.querySelector('.t-mistakes__description'); //li
-    let resultTest = document.querySelector('#result-test');
-    let rightAnswer = 0;
-    let result;
+  let mistake = document.querySelector('.t-mistakes__numbers'); //span
+  let question = document.querySelector('.t-mistakes__description'); //li
+  let resultTest = document.querySelector('#result-test');
+  let rightAnswer = 0;
+  let result;
 
-    arrRadio.map(answer=>{
-        if(answer.checked) {
+  arrRadio.map(answer => {
+    if (answer.checked) {
 
-           if(answer.value === templateData[3].correctAnswer) {
-               console.log('Ответ верный!');
-               mistake.classList.remove('t-mistakes__numbers-wrong');
-               question.lastElementChild.remove();
-               rightAnswer++;
-           }
-           else {
-               console.log('Ответ не верен');
-           }
-       }
-    });
+      if (answer.value === templateData[3].correctAnswer) {
+        console.log('Ответ верный!');
+        mistake.classList.remove('t-mistakes__numbers-wrong');
+        question.lastElementChild.remove();
+        rightAnswer++;
+      } else {
+        console.log('Ответ не верен');
+      }
+    }
+  });
 
-    result = Math.floor((rightAnswer/1)*10);  // если вопросов много, тогда вместо единицы подставляем кол-во вопросов т.е. длинну массива
-    resultTest.innerHTML = `${result}/10 баллов`;
+  result = Math.floor((rightAnswer / 1) * 10); // если вопросов много, тогда вместо единицы подставляем кол-во вопросов т.е. длинну массива
+  resultTest.innerHTML = `${result}/10 баллов`;
 }
 
 function checkratio(data) {
@@ -137,7 +150,10 @@ function checkratio(data) {
       let next = check[i].nextSibling;
       next = next.previousSibling.defaultValue;
       localStorage.setItem(data.id, next);
-      console.log(next);
+      console.log("Id of page " + data.id)
+
+      let page = document.getElementsByClassName('t-navigation__number');
+
     }
 
   }
@@ -145,7 +161,57 @@ function checkratio(data) {
 
 let numberQues = document.getElementsByClassName("t-navigation__number");
 for (var i = 0; i < numberQues.length; i++) {
-  numberQues[i].addEventListener("click", function () {
+  numberQues[i].addEventListener("click", function() {
     console.log(numberQues[i].value);
-  })
+  });
 }
+
+function getLenghtTests(c) {
+  fetch(c)
+    .then(response => {
+      if (response.ok) return response.json();
+      throw new Error("Error");
+      return data;
+
+
+    }).then(data => {
+
+
+        let pages = document.querySelector('.t-navigation__pagination');
+        for (var z = 0; z < data.length; z++) {
+          let li = document.createElement('li');
+          li.innerHTML = z + 1;
+          pages.append(li);
+          li.classList = "t-navigation__number";
+          if (localStorage.getItem(li.innerHTML)) {
+            li.classList.add("t-navigation__number--blue");
+          }
+        }
+        let numberPages = document.getElementsByClassName('t-navigation__number');
+
+        for (let x = 0; x < numberPages.length; x++) {
+          numberPages[x].addEventListener("click", function() {
+
+            let nowTest = document.querySelector('.t-question__task').innerHTML;
+
+            let newId = nowTest[nowTest.length-1];
+            let check = document.getElementsByClassName('t-answers__item');
+            for (var y = 0; y < check.length; y++) {
+              if (check[y].checked) {
+                let next = check[y].nextSibling;
+                next = next.previousSibling.defaultValue;
+
+                localStorage.setItem(newId, next);
+              }
+            }
+              //let rezTest = localStorage.getItem(i + 1);
+              // console.log("значение ли " + this.innerHTML);
+              let urlNext = url + "/" + this.innerHTML;
+
+              getTestJson(urlNext);
+            });
+            }
+
+
+              });
+            }
