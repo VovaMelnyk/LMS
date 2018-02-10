a65953a4fe13c0d393ab245f9991e3b0374cc773
Sergii Tymchenko <p70smart@gmail.com>
2018-02-10 20:22:37 +0200

    mudul-window_and_corrected_photo-profile

--- gulp/src/js/script.js
+++ gulp/src/js/script.js
@@ -1,8 +1,10 @@
-document.querySelector('.c-header-photo-profile__photo').onclick = ()=>{
-	let formLogin = document.querySelector('.c-header-photo-profile__form-login');
-	if(formLogin.style.display == 'none'){
-        formLogin.style.display = 'block';
-    } else {
-		formLogin.style.display = 'none';
-    }
+let formLogin = document.querySelector('.c-header-photo-profile__form-profile');
+let photo = document.querySelector('.c-header-photo-profile__photo');
+let close = document.querySelector('.c-header-photo-profile__form-profile-close');
+photo.onclick = ()=>{
+	formLogin.style.display = 'block';
+	close.onclick = ()=>{
+		formLogin.style.display = 'none'
+	};
+	
 };
