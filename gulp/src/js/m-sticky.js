b7cbd5e9136deb56a2accd1319d0d8a22bd10eec
yevheniigurinets <yevheniigurinets@gmail.com>
2018-02-10 18:21:40 +0200

    fixed bug with sticky

--- gulp/src/js/m-sticky.js
+++ gulp/src/js/m-sticky.js
@@ -8,20 +8,59 @@ let btnUp = document.querySelector('#invBtnUp');
 let headerText = document.querySelector('.m-header__title');
 let headerTextSm = document.querySelector('.caption__theme');
 
-function myFunction() {
+function myFunction(folder,clock,sticky,stickys,btnUp,headerText,headerTextSm) {
   if (window.pageYOffset >= sticky) {
-    folder.classList.add("m-sticky");
-    btnUp.classList.remove("invisible");
-    headerText.classList.add("m-slall-text");
-    headerTextSm.classList.add("m-slall-text");
+    if (folder) {
+      folder.classList.add("m-sticky");
+    }
+    if (btnUp) {
+      btnUp.classList.remove("invisible");
+    }
+    if (headerText) {
+      headerText.classList.add("m-slall-text");
+    }
+    if (headerTextSm) {
+      headerTextSm.classList.add("m-slall-text");
+    }
   } if (window.pageYOffset >= stickys) {
-    clock.classList.add("m-flex")
+    if (clock) {
+      clock.classList.add("m-flex")
+    }
   } else {
-    folder.classList.remove("m-sticky");
-    clock.classList.remove("m-flex");
-    btnUp.classList.add("invisible");
-    headerText.classList.remove("m-slall-text");
-    headerTextSm.classList.remove("m-slall-text");
+    if (folder) {
+      folder.classList.remove("m-sticky");
+    }
+    if (clock) {
+      clock.classList.remove("m-flex");
+    }
+    if (btnUp) {
+      btnUp.classList.add("invisible");
+    }
+    if (headerText) {
+      headerText.classList.remove("m-slall-text");
+    }
+    if (headerTextSm) {
+      headerTextSm.classList.remove("m-slall-text");
+    }
   }
 }
-window.onscroll = () => { myFunction() };
+window.onscroll = () => { myFunction(folder,clock,sticky,stickys,btnUp,headerText,headerTextSm) };
+
+if (document.getElementsByClassName('m-btn-test') ) {
+  document.getElementsByClassName('m-btn-test')[0].addEventListener('click', () => {
+    
+    setTimeout( () => {
+      const folder = document.querySelector('#m-title');
+      const clock = document.querySelector('#m-timers');
+      const sticky = folder.getBoundingClientRect().top + window.pageYOffset;
+      const stickys = folder.getBoundingClientRect().top + window.pageYOffset;
+      const btnUp = document.querySelector('#invBtnUp');
+      const headerText = document.querySelector('.m-header__title');
+      const headerTextSm = document.querySelector('.caption__theme');
+
+      window.onscroll = () => { 
+        myFunction(folder,clock,sticky,stickys,btnUp,headerText,headerTextSm)
+       };
+      }, 1000)
+  })
+}
