b6510a31426140e33feaf61244d0b6383dc4dfe2
Vladyslav Lubeznyy <avinean@gmail.com>
2018-02-11 10:55:19 +0300

    gulp fix

---
+++ public/js/profile.js
@@ -0,0 +1,80 @@
+if (!localStorage.getItem('lms_img') &&
+    !localStorage.getItem('lms_name') &&
+    !localStorage.getItem('lms_email')
+    ) {
+    window.location = 'http://localhost:3000/index.html';
+}
+
+
+var img = localStorage.getItem('lms_img');
+var name = localStorage.getItem('lms_name');
+var lastName = localStorage.getItem('lms_lastName');
+var email = localStorage.getItem('lms_email');
+var facebook = localStorage.getItem('lms_facebook');
+var google = localStorage.getItem('lms_google');
+var linkedin = localStorage.getItem('lms_linkedin');
+
+document.querySelector('#profile_photo').setAttribute('src', 'img/users/' + img);
+document.querySelector('#header_photo-profile').setAttribute('src', 'img/users/' + img);
+document.querySelector('#user-name').innerHTML = name + ' ' + lastName + '&#8194;';
+document.querySelector('#user-email').innerHTML = email; 
+document.querySelector('#user-facebook').setAttribute('href', facebook); 
+document.querySelector('#user-google').setAttribute('href', google); 
+document.querySelector('#user-linkedin').setAttribute('href', linkedin); 
+
+function slider() {
+
+    var counter = 0,
+    left = 0,
+    right = 0,
+    margin = 0;
+
+    var slides = [];
+    
+    axios('http://localhost:3000/users').then(
+        function (data) {
+
+           for(var i=0; i<data.data.length; i++){
+              if(data.data[i].group == "FrontEnd_1"){
+                 slides.push(`
+                     <div class="slider-item">
+                     <p class="item-img"><img src="img/users/${data.data[i].img}" alt=""></p>
+                     <p class="item-name">${data.data[i].name} ${data.data[i].lastName}</p>
+                     <p class="item-score">${data.data[i].grade}</p>
+                     </div>`);
+                 counter++;
+             }
+         }
+
+         document.querySelector('.slider-stripe').innerHTML = slides.join('');
+         document.querySelector('.arrowLeft').addEventListener('click', left);
+         document.querySelector('.arrowRight').addEventListener('click', right);
+
+         function left() {
+
+            margin -= 150;
+
+            if (margin < -150 * (counter -5 )) {
+                margin = 0
+            }
+            document.querySelector('.slider-stripe').style.marginLeft = margin + 'px';
+        }
+
+        function right() {
+
+            margin += 150;
+            if (margin > 0) {
+                margin = -150 * (counter -5 );
+            }
+
+            document.querySelector('.slider-stripe').style.marginLeft = margin + 'px';
+        }
+    });
+}
+
+slider();
+
+document.querySelector('#header_photo-profile').addEventListener('click', e => {
+    localStorage.clear();
+    window.location = 'http://localhost:3000/index.html';
+})
---
+++ public/js/profile2.js
@@ -0,0 +1,91 @@
+if (!localStorage.getItem('lms_img') &&
+    !localStorage.getItem('lms_name') &&
+    !localStorage.getItem('lms_email')
+) {
+    window.location = 'http://localhost:3000/index.html';
+}
+
+
+var img = localStorage.getItem('lms_img');
+var name = localStorage.getItem('lms_name');
+var lastName = localStorage.getItem('lms_lastName');
+var email = localStorage.getItem('lms_email');
+var facebook = localStorage.getItem('lms_facebook');
+var google = localStorage.getItem('lms_google');
+var linkedin = localStorage.getItem('lms_linkedin');
+var open = localStorage.getItem('lms_profile');
+
+
+document.querySelector('#profile_photo').setAttribute('src', 'img/users/' + img);
+document.querySelector('#header_photo-profile').setAttribute('src', 'img/users/' + img);
+document.querySelector('#user-name').value = name;
+document.querySelector('#user-secondname').value = lastName;
+document.querySelector('#user-email').value = email; 
+document.querySelector('#user-facebook').value = facebook; 
+document.querySelector('#user-google').value = google; 
+document.querySelector('#user-linkedin').value = linkedin;
+document.querySelector('.profile-public-icon').setAttribute('src', open ? 'img/open.svg' : 'img/close.svg')
+document.querySelector('.profile-public-message').innerHTML = open ? 'I want my profile to be privat' : 'I want my profile to be public';
+document.querySelector('.profile-public-message').style.color = open ? '#00ff00' : '#ff0000';
+
+document.querySelector('#old-pass').addEventListener('change', (e) => {
+	let pass = e.target.value;
+	let pass2 = localStorage.getItem('lms_pass');
+
+	if (pass != pass2) {
+		document.querySelector('.alert').innerHTML = 'Неверный старый пароль';
+	}
+	else {
+		document.querySelector('.alert').innerHTML = '';
+	}
+}) 
+
+document.querySelector('#new-pass').addEventListener('change', (e) => {
+
+	let pass = document.querySelector('#new-pass').value;
+
+	if (pass.search(/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g)) {
+		document.querySelector('.alert').innerHTML = 'Пароль слишком короткий  <br> или недоступный символ';
+	}
+	else {
+		document.querySelector('.alert').innerHTML = '';
+	}
+});
+
+document.querySelector('#repeat-new-pass').addEventListener('change', function(e) {
+
+	let pass = document.querySelector('#new-pass').value;
+	let pass2 = document.querySelector('#repeat-new-pass').value;
+
+	if (pass.search(/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g)) {
+		document.querySelector('.alert').innerHTML = 'Пароль слишком короткий <br> или недоступный символ';
+	}
+	else {
+		document.querySelector('.alert').innerHTML = '';
+	}
+
+	if (pass != pass2) {
+		document.querySelector('.alert').innerHTML = 'Пароли не совпадают';
+	}
+	else {
+		document.querySelector('.alert').innerHTML = '';
+	}
+}) 
+
+function publicFunction() {
+	return function() {
+		document.querySelector('.profile-public-icon').setAttribute('src', !open ? 'img/open.svg' : 'img/close.svg')
+		document.querySelector('.profile-public-message').innerHTML = !open ? 'I want my profile to be privat' : 'I want my profile to be public';
+		document.querySelector('.profile-public-message').style.color = !open ? '#00ff00' : '#ff0000';
+		localStorage.setItem('lms_profile', open ? '' : 1);
+		open = !open;
+	}
+}
+
+document.querySelector('.profile-public-icon').addEventListener('click', publicFunction());
+
+
+document.querySelector('#header_photo-profile').addEventListener('click', e => {
+    localStorage.clear();
+    window.location = 'http://localhost:3000/index.html';
+})
